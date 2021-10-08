5b1952b2f3273a85eddf48128a83040241bc6429
Silkage <silkage@silkage.net>
2021-10-08 10:30:17 +0800

    fix: rename workspace command

--- cmd/workspace/workplace.go
+++ cmd/workspace/workplace.go
@@ -3,7 +3,7 @@ package workspace
 import "github.com/urfave/cli/v2"
 
 var Command = &cli.Command{
-	Name:    "workplace",
+	Name:    "workspace",
 	Aliases: []string{"w"},
 	Usage:   "Commands related to the management workspace.",
 	Subcommands: []*cli.Command{
